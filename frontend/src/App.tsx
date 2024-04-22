8c3aee0b093816cd68f2d81bad72655686d45c5b
Ana Popovic <franjetina@gmail.com>
2024-04-22 13:47:14 +0200

    feat: copy data function implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import axios from 'axios';
+import { LuCopy } from "react-icons/lu";
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
   const [converting, setConverting] = useState(false);
   const [jsonData, setJsonData] = useState<string | null>(null);
+  const [converted, setConverted] = useState(false);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files && e.target.files[0];
     if (selectedFile) {
       setFile(selectedFile);
+      setConverted(false);
     }
   };
 
@@ -27,12 +30,16 @@ function App() {
           }
         });
         console.log(response.data.data);
-        setJsonData(response.data.data);
+        setTimeout(() => {
+          setJsonData(response.data.data);
+          setConverting(false);
+          setConverted(true);
+        }, 3000);
+
       } catch (error) {
         console.error('Error converting file:', error);
-      } setTimeout(() => {
-        setConverting(false); 
-      }, 3000);
+        setConverting(false);
+      }
     }
   };
 
@@ -73,12 +80,23 @@ function App() {
     }
   };
 
+  const handleCopy = () => {
+    if (jsonData) {
+      navigator.clipboard.writeText(jsonData)
+        .then(() => {
+          alert("JSON data copied to clipboard!");
+        })
+        .catch((error) => {
+          console.error("Error copying JSON data:", error);
+        });
+    }
+  };
+
 
   return (
     <>
       <div>
-      <div className='flex flex-col gap-4 border border-blue-300 rounded-md p-12'>
-        {!jsonData && (
+        <div className='flex flex-row gap-4 border border-blue-300 rounded-md p-12'>
           <div className="relative">
             <input
               type="file"
@@ -91,32 +109,39 @@ function App() {
               Choose File
             </button>
           </div>
-        )}
-        {file && (
-          <p className="mt-2">{file.name}</p>
-        )}
-        
-          <button onClick={handleConvert} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          {converting ? "Converting..." : "Convert to JSON"}
+          {file && (
+            <p className="mt-2">{file.name}</p>
+          )}
+
+          <button
+            onClick={handleConvert}
+            disabled={converting || converted}
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${converted ? 'hidden' : ''}`}>
+            {converting ? "Converting..." : "Convert to JSON"}
           </button>
-          {jsonData && (
-            <button onClick={handleDownload} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          {jsonData && !converting && (
+            <button
+              onClick={handleDownload}
+              disabled={converting && !jsonData}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
               Download JSON
             </button>
           )}
-      </div>
-      {jsonData && (
-        <div className="mt-8">
-        <p className="text-lg font-bold">Converted JSON:</p>
-        <div className="mt-2 text-start overflow-x-auto p-4 rounded-md border border-gray-300" style={{ maxHeight: '300px' }}>
-          <pre>
-            {jsonData}
-          </pre>
         </div>
+        {jsonData && !converting && (
+          <div className="mt-8 relative">
+            <p className="text-lg font-bold">Converted JSON:</p>
+            <div className="mt-2 text-start overflow-x-auto p-4 rounded-md border border-gray-300 relative" style={{ maxHeight: '300px' }}>
+              <pre>
+                {jsonData}
+              </pre>
+            </div>
+            <button onClick={handleCopy} className="absolute top-12 right-4 py-2 px-4 rounded bg-transparent">
+              <LuCopy />
+            </button>
+          </div>
+        )}
       </div>
-      
-      )}
-    </div>
     </>
   )
 }
