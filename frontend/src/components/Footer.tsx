b34262715395ebc3e756be9f53d144a79b36f630
Ana Popovic <franjetina@gmail.com>
2024-05-02 09:10:30 +0200

    footer added

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,9 @@ export function Footer() {
     return (
         <>
         <div className='w-full flex flex-col justify-center items-center border-2 border-t-[#407D8B] drop-shadow-xl fixed bottom-0 bg-[#FCFCF9] p-4'>
-            <div className="">footer</div>
+            <div className="text-[#407D8B]">
+                &copy; Ana Popovic | 2024 | All rights reserved
+            </div>
         </div>
         </>
     )
